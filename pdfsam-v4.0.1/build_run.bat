21800b80627003e1a927266cd21ccd4ce27540e9
Oscar Chaparro <ojcch1@gmail.com>
2021-09-14 22:48:55 -0400

    changes to fix dependency issues during project building

--- pdfsam-v4.0.1/build_run.bat
+++ pdfsam-v4.0.1/build_run.bat
@@ -1,6 +1,7 @@
 REM change the JAVA_HOME path (JRE/JDK 11+)
 set JAVA_HOME=C:\Program Files\Java\jdk-12.0.2
 
+call mvn install:install-file "-Dfile=extra_libs\sejda-injector-1.0.2.jar" "-DgroupId=org.sejda" "-DartifactId=sejda-injector" "-Dversion=1.0.2" "-Dpackaging=jar" "-Dsources=extra_libs\sejda-injector-1.0.2-sources.jar"
 call mvn install -DskipTests -Drelease -Dgpg.skip
 
 call "%~dp0\pdfsam-basic\target\assembled\pdfsam.exe"
--- pdfsam-v4.0.1/build_run.sh
+++ pdfsam-v4.0.1/build_run.sh
@@ -1,6 +1,10 @@
 #change the JAVA_HOME path (JRE/JDK 11+)
 export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
 
+mvn install:install-file "-Dfile=extra_libs/sejda-injector-1.0.2.jar" "-DgroupId=org.sejda" "-DartifactId=sejda-injector" "-Dversion=1.0.2" "-Dpackaging=jar" "-Dsources=extra_libs/sejda-injector-1.0.2-sources.jar"
 mvn install -DskipTests -Drelease -Dgpg.skip
 
+#it is possible the sh script is created using LF+CR characters for new lines.
+#if this is the case, we need to convert LF+CR to LF, by running the following command (dos2unix need to be installed first):
+#dos2unix ./pdfsam-basic/target/assembled/bin/pdfsam.sh
 ./pdfsam-basic/target/assembled/bin/pdfsam.sh
