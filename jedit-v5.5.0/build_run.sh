230b04c76ee6279f518da45172d273fc1220040f
Oscar Chaparro <ojcch1@gmail.com>
2022-09-14 17:13:38 -0400

    update to build scripts

--- jedit-v5.5.0/build_run.sh
+++ jedit-v5.5.0/build_run.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
+
 # change the JAVA_HOME path (JDK/JRE 8)
-export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-8.jdk/Contents/Home"
 
 ant retrieve build run
